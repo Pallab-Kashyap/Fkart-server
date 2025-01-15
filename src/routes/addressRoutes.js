937f5b0b5ba79fa55fec346e9d7e1748674ea84c
Amitsingh121 <amitksingh7779@gmail.com>
2025-01-15 17:18:09 +0530

    changes in addressController and userAddress

--- src/routes/addressRoutes.js
+++ src/routes/addressRoutes.js
@@ -1,18 +1,16 @@
 
 import express from 'express';
 const router = express.Router();
-// import addressController from '../controllers/addressController.js';
-// import  addressController from '../controllers/addressController';
 // add ------------------------------------------------------------------------------------done
 // fetch-----------------------------------------------------------------------------------done
 // update----------------------------------------------------------------------------------done
 // remove----------------------------------------------------------------------------------done
-// set default
+// set default ----------------------------------------------------------------------------done
 
 import { createAddress, getAddresses, updateAddress, deleteAddress, setDefaultAddress } from '../controllers/addressController.js';
 import auth from '../middlewares/authMiddleware.js';
 
-router.route('/').post( auth, createAddress)
+router.route('/').post( auth, createAddress) 
 //                 .get('/', getAddresses)
 // router.route('/:id').put( updateAddress)
 //                     .delete('/:id', deleteAddress);
