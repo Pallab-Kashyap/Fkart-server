857de0c191596e04647ca33a2a978e035baa8e69
Pallab <pallabkshyp@gmail.com>
2025-02-05 01:01:58 +0530

    updated response format

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -3,6 +3,7 @@ import ApiError from "../utils/APIError.js";
 const errorHandler = (err, req, res, next) => {
   if (err instanceof ApiError) {
     return res.status(err.statusCode).json({
+      status: false,
       message: err.message,
       ...(process.env.NODE_ENV === 'development' && { stack: err.stack }), 
     });
@@ -12,17 +13,20 @@ const errorHandler = (err, req, res, next) => {
     // Handle Sequelize Unique Constraint Error
     if (err.name === 'SequelizeUniqueConstraintError') {
       return res.status(409).json({
+        status: false,
         message: 'Duplicate value detected. Please check your input.',
       });
     }
     // Handle Other Sequelize Errors
     return res.status(400).json({
+      status: false,
       message: 'Database error occurred.',
       details: err.message,
     });
   }
 
   return res.status(500).json({
+    status: false,
     message: err.message,
     ...(process.env.NODE_ENV === 'development' && { stack: err.stack }), 
   });
--- src/routes/squareRoutes.js
+++ src/routes/squareRoutes.js
@@ -8,6 +8,19 @@ const router = Router();
 router.get("/authorize", authorizeSquare)
 router.get("/callback", squareCallback)
 
+//Webhook
+// router.get("/webhook/notif", (req, res) => {
+//   res.status(200).send("Webhook received get")
+// })
+// router.post("/webhook/notif", (req, res) => {
+//   console.log(req.body,)
+//   res.status(200).send("Webhook received post")
+// })
+// router.post("/webhook", (req, res) => {
+//   console.log(req.body)
+//   res.send("Webhook received")
+// })
+
 //Interact with Square API
 router.get("/items", fetchSquareCatalogList)
 
