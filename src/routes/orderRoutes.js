938a8afdba4c788f353ad7b6042efab313216256
Amitsingh121 <amitksingh7779@gmail.com>
2025-02-13 21:56:39 +0530

    add order routes

---
+++ src/routes/orderRoutes.js
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+import { createOrder, 
+    createOrderFromProduct,
+     getOrder,
+      getUserOrders 
+    } from '../controllers/orderController.js';
+import auth from '../middlewares/authMiddleware.js';
+
+const router = Router();
+
+router.post('/create', auth, createOrder);
+router.post('/create-from-product', auth, createOrderFromProduct);
+router.get('/:id', auth, getOrder);
+router.get('/user/orders', auth, getUserOrders);
+
+export default router;  
